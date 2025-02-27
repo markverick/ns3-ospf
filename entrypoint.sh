d292cfee1c7e8025f22233404dbf27a146836996
Sirapop Theeranantachai <sirapop741@gmail.com>
2025-02-27 09:58:19 +0000

    build: fixed github workflow output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
 cd /usr/local/app/ns-3
-./waf --run=ospf-metric >> $GITHUB_OUTPUT
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "Warning: GITHUB_OUTPUT is not set. Output will not be saved."
+else
+  ./waf --run=ospf-metric >> "$GITHUB_OUTPUT"
+fi
