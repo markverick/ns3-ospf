72e5d817b03aa7917e0db6594266cba809c67e5e
Sirapop Theeranantachai <sirapop741@gmail.com>
2025-03-10 20:45:22 +0000

    helper: more functions in runtime-helper

--- ospf/examples/ospf-grid-area.cc
+++ ospf/examples/ospf-grid-area.cc
@@ -30,6 +30,7 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/ospf-app-helper.h"
 #include "ns3/ospf-app.h"
+#include "ns3/ospf-runtime-helper.h"
 
 #include <cassert>
 #include <fstream>
@@ -48,84 +49,6 @@ const uint32_t NUM_STRIPES = 3;
 const uint32_t GRID_HEIGHT = 22;
 const uint32_t GRID_WIDTH = STRIPE_WIDTH * NUM_STRIPES;
 const uint32_t SIM_SECONDS = 100;
-
-// Fill static routes with
-void
-SetLinkDown (Ptr<NetDevice> nd)
-{
-  Ptr<RateErrorModel> pem = CreateObject<RateErrorModel> ();
-  pem->SetRate (1.0);
-  nd->SetAttribute ("ReceiveErrorModel", PointerValue (pem));
-}
-
-void
-SetLinkError (Ptr<NetDevice> nd)
-{
-  Ptr<RateErrorModel> pem = CreateObject<RateErrorModel> ();
-  pem->SetRate (0.005);
-  nd->SetAttribute ("ReceiveErrorModel", PointerValue (pem));
-}
-
-void
-SetLinkUp (Ptr<NetDevice> nd)
-{
-  Ptr<RateErrorModel> pem = CreateObject<RateErrorModel> ();
-  pem->SetRate (0.0);
-  nd->SetAttribute ("ReceiveErrorModel", PointerValue (pem));
-}
-
-void
-CompareLsdb (NodeContainer nodes)
-{
-  NS_ASSERT (nodes.GetN () > 0);
-  Ptr<OspfApp> app = DynamicCast<OspfApp> (nodes.Get (0)->GetApplication (0));
-  uint32_t hash = app->GetLsdbHash ();
-
-  for (uint32_t i = 1; i < nodes.GetN (); i++)
-    {
-      app = DynamicCast<OspfApp> (nodes.Get (i)->GetApplication (0));
-      if (hash != app->GetLsdbHash ())
-        {
-          std::cout << "[" << Simulator::Now () << "] LSDBs mismatched" << std::endl;
-          return;
-        }
-    }
-  std::cout << "[" << Simulator::Now () << "] LSDBs matched: " << hash << std::endl;
-  ;
-  return;
-}
-
-void
-VerifyNeighbor (NodeContainer allNodes, NodeContainer nodes)
-{
-  NS_ASSERT (nodes.GetN () > 0);
-  bool match = true;
-  for (uint32_t i = 0; i < nodes.GetN (); i++)
-    {
-      Ptr<OspfApp> app = DynamicCast<OspfApp> (nodes.Get (i)->GetApplication (0));
-      for (auto &pair : app->GetLsdb ())
-        {
-          if (pair.second.second->GetNLink () !=
-              allNodes.Get (app->GetNode ()->GetId ())->GetNDevices () - 1)
-            {
-              std::cout << "[" << Simulator::Now () << "] LSDB entry [" << Ipv4Address (pair.first)
-                        << "] of node [" << i << "] is incorrect ("
-                        << pair.second.second->GetNLink ()
-                        << " != " << allNodes.Get (app->GetNode ()->GetId ())->GetNDevices () - 1
-                        << ")" << std::endl;
-              match = false;
-              for (uint32_t j = 0; j < pair.second.second->GetNLink (); j++)
-                {
-                  std::cout << "  " << Ipv4Address (pair.second.second->GetLink (j).m_linkId);
-                }
-              std::cout << std::endl;
-            }
-        }
-    }
-  if (match)
-    std::cout << "[" << Simulator::Now () << "] LSDB entries correct" << std::endl;
-  return;
-}
 
 int
 main (int argc, char *argv[])
